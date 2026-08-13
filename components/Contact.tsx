755ff43df6ab49a3010856c8516a6560e0246791
Oktay Dolasmaz <oktay@gmail.com>
2026-08-13 18:55:04 +0300

    Oktay Studio yazı stili güncellendi

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -203,7 +203,7 @@ export default function Contact() {
 <div className="mt-16 pt-8 border-t border-gray-200 text-center">
   <p className="text-gray-400 text-sm">
     Web Tasarım & Geliştirme:{" "}
-    <span className="text-orange-900 font-black-text-x3">
+    <span className="text-orange-500 font-black-text-x3">
       Oktay Studio
     </span>
   </p>
